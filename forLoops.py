a7584f0cb635404df962f01d4a6a73a31265f219
Aijaz Ahmad War <166946209+Aijaz-09@users.noreply.github.com>
2025-12-24 21:03:24 +0530

    Add files via upload

---
+++ forLoops.py
@@ -0,0 +1,18 @@
+# for loops = execute a block of code a Fixed Number of times
+#             You can iterate over a range, sequence, string, etc
+import time
+for x in reversed(range(1,6)):
+    print(x)
+    time.sleep(1)
+print("Hello Python")
+
+# storages = "0128-0256-0512-1024"
+# for x in storages :
+#     print(x)
+
+
+# for x in range(1,21):
+#     if x == 13:
+#         continue
+#     else:
+#         print(x)
