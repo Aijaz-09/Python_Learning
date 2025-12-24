e0777b9c4e0d5aa4d9e64da9985364ed90f4e17f
Aijaz Ahmad War <166946209+Aijaz-09@users.noreply.github.com>
2025-12-25 00:42:41 +0530

    Add files via upload

---
+++ ListsSetsTuples.py
@@ -0,0 +1,28 @@
+# collections = single "variable" used to store multiple values
+# list = [] ordered and changeable. duplicates OK
+# sets = {} unordered and immutable, but Add/Remove OK. No duplicates
+# tuples = () ordered and unchangeable. duplicates OK. FASTER
+
+fruits = ("apple", "orange", "banana", "coconut") # tuple
+
+print(dir(fruits))
+# print(help(fruits))
+# print(len(fruits))
+# print("apple" in fruits)
+
+# fruits[1] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0, "pineapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.sort()
+# fruits.reverse() #First sort, then reverse = reverse alphabetical order
+# fruits.clear()
+# print(fruits.index("apple"))
+# print(fruits.count("banana"))
+# print(fruits)
+# fruits.pop()
+
+# print(fruits)
+
